ba4b1dbec52ea4ad587fea43ef92765552aea829
Batradz-T <tasoevbatradz@yandex.ru>
2020-12-05 09:13:24 +0300

    Решение задачи 1.4.final.5

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -4,12 +4,9 @@ import jm.task.core.jdbc.model.User;
 import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 
-import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.mapping.MetadataSource;
-import org.hibernate.metamodel.Metadata;
-import org.hibernate.metamodel.MetadataSources;
+
 
 import java.sql.*;
 
@@ -29,7 +26,16 @@ public class Util {
         if (sessionFactory == null) {
             try {
 
-                Configuration configuration = new Configuration().configure();
+                Configuration configuration = new Configuration();
+                configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+                configuration.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/usersdb");
+                configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+                configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+                configuration.setProperty("hibernate.connection.username", "root");
+                configuration.setProperty("hibernate.connection.password", "1234");
+                configuration.setProperty("hibernate.show_sql", "true");
+
+                configuration.addPackage( "jm.task.core.jdbc.model" );
                 configuration.addAnnotatedClass(User.class);
 
                 StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
